bcede5892cb3f9baa61a7a71df505e2127c253e8
Federico <federico.capelli8@studio.unibo.it>
2025-11-23 23:48:51 +0100

    add the final part of SimpleGUI.java in exercise 92

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -3,23 +3,29 @@ package it.unibo.mvc;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
- * 
+ *
  */
 
 public final class SimpleGUI {
- 
+
     private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame("Simple GUI");
+    private final Controller controller = new Controller();
 
     /**
-     * 
+     *
      */
     public SimpleGUI() {
         final JPanel panel = new JPanel(new BorderLayout());
@@ -29,6 +35,17 @@ public final class SimpleGUI {
         panel.add(saveButton, BorderLayout.SOUTH);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.saveOnFile(textArea.getText());
+                } catch (final IOException error) {
+                    JOptionPane.showMessageDialog(null, error.getMessage(), "Occured an error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
     }
 
     private void display() {
