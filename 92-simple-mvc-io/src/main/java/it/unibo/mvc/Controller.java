b71769ac668a296c9dbcac650b940def31d784ad
Federico <federico.capelli8@studio.unibo.it>
2025-11-23 11:52:34 +0100

    Add Controller.java

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,48 @@
 package it.unibo.mvc;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 /**
  * Application controller. Performs the I/O.
  */
+
 public class Controller {
 
+    private final String home = System.getProperty("user.home");
+    private final String separator = System.getProperty("file.separator");
+    private File fileCurrent = new File(home + separator + "output.txt");
+
+    /**
+     * @param file set a new file as current file
+     */
+    public void setAsCurrentFile(final File file) {
+        this.fileCurrent = file;
+    }
+
+    /**
+     * @return the current file
+     */
+    public File getFile() {
+        return this.fileCurrent;
+    }
+
+    /**
+     * @return the path of the current file
+     */
+    public String getFilePath() {
+        return this.fileCurrent.getPath();
+    }
+
+    /**
+     * @param message save a text on current file
+     * 
+     * @throws IOException if the saving fails
+     */
+    public void saveOnFile(final String message) throws IOException {
+        try (PrintStream ps = new PrintStream(this.fileCurrent, StandardCharsets.UTF_8)) {
+            ps.print(message);
+        }
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,44 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
-public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+public final class SimpleGUI {
+ 
+    private static final int PROPORTION = 5;
+    private final JFrame frame = new JFrame("Simple GUI");
+
+    public SimpleGUI() {
+        final JPanel panel = new JPanel(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        panel.add(textArea);
+        final JButton saveButton = new JButton("Save");
+        panel.add(saveButton, BorderLayout.SOUTH);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
