1efb5f70c3c5dc4f78579c7808a754c2337aef35
Federico <federico.capelli8@studio.unibo.it>
2025-11-23 14:46:41 +0100

    Add Controller.java in exercise 93

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,29 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * @param nextStringToPrint set a new string as nextString
+     */
+    void setNextString(String nextStringToPrint);
+
+    /**
+     * @return next String to print
+     */
+    String getNextString();
+
+    /**
+     * @return the history of printed strings
+     */
+    List<String> getHistoryPrint();
+
+    /**
+     *
+     */
+    void printCurrentString();
 }
