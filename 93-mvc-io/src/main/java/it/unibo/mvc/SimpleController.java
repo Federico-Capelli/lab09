5e9cac54d9b2454102955d206dc7b189cd2b4512
Federico <federico.capelli8@studio.unibo.it>
2025-11-23 14:48:23 +0100

    Add SimpleController in exercise 93

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * 
+ *
  *
  */
 public final class SimpleController implements Controller {
 
+    private final List<String> stringHistory = new LinkedList<>();
+    private String nextString;
+
+    @Override
+    public void setNextString(final String nextString) {
+        if (nextString == null) {
+            throw new IllegalArgumentException("Next String cannot be null");
+        }
+        this.nextString = nextString;
+    }
+
+    @Override
+    public String getNextString() {
+       return this.nextString;
+    }
+
+    @Override
+    public List<String> getHistoryPrint() {
+       return Collections.unmodifiableList(stringHistory);
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (this.nextString == null) {
+            throw new IllegalStateException("Current String is unset");
+        }
+        System.out.println(this.nextString); //NOPMD: allowed
+        stringHistory.add(nextString);
+    }
 }
